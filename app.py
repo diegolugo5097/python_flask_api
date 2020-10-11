d652d7c75c0eefcb2a40b4b92e908f838e4fb93b
diegolugo5097 <diegolugot@hotmail.com>
2020-10-11 16:55:35 -0500

    obtecnion de objeto por parametro

--- app.py
+++ app.py
@@ -2,9 +2,30 @@ from flask import Flask, jsonify
 from products import products
 app = Flask(__name__)
 
-@app.route('/ping')
+
+@app.route('/ping', methods=['GET'])
 def ping():
     return jsonify({'message': 'pong'})
+
+# Obtencion del objeto products
+
+
+@app.route('/products', methods=['GET'])
+def get_products():
+    return jsonify({"message": "Product's list", "products": products})
+
+# obtencion del objeto por parametro
+
+
+@app.route('/products/<product_name>')
+def search_product(product_name):
+    productsFound = [
+        product for product in products if product['name'] == product_name
+    ]
+    if len(productsFound) > 0:
+        return jsonify({"product": productsFound[0]})
+    return jsonify({'message': 'product not found'})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
